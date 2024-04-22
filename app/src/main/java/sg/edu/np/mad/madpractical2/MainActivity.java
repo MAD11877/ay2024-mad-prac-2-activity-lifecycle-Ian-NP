d727b7e96a7e6236d05971c29757da4fa765a2ee
Ian <iancheif117@gmail.com>
2024-04-22 19:01:24 +0800

    Practical2

---
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -0,0 +1,98 @@
+package sg.edu.np.mad.madpractical2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+// Add imports here
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize a new User object
+        User user = new User("John Doe", "MAD Developer", 1, false);
+
+        // Get the TextViews and Button from the layout
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnfollow = findViewById(R.id.followbtn);
+
+        // Set the TextViews with the User's name, description and default button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnfollow.setText("Follow");
+
+        final boolean[] followed = {false};
+        btnfollow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (!followed[0]){
+                    btnfollow.setText("Follow");
+                    followed[0] = true;
+                }
+                else if (followed[0]){
+                    btnfollow.setText("Unfollow");
+                    followed[0] = false;
+                }
+            }
+        });
+
+        // Go to XML to locate the buttons
+//        Button button1 = findViewById(R.id.button1);
+//        Button button2 = findViewById(R.id.button2);
+//        Button button3 = findViewById(R.id.button3);
+//        Button button4 = findViewById(R.id.button4);
+//
+//        //set the click Listener
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.i(TAG, "Button 1 is pressed.");
+//            }
+//        });
+//
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.i(TAG, "Button 2 is pressed.");
+//            }
+//        });
+//
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.i(TAG, "Button 3 is pressed.");
+//            }
+//        });
+//
+//        button4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.i(TAG, "Button 4 is pressed.");
+//            }
+//        });
+    }
+}
